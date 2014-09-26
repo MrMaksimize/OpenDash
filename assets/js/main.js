0d0cfb86d85a9050766d8e5d15a6385362e85593
Maksim Pecherskiy <max@maksimize.com>
2014-09-26 16:58:13 -0700

    Some abstraction of panel builds.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,33 +15,56 @@ $(function() {
      */
     gapi.analytics.auth.on('success', function() {
       $('html').addClass('is-authorized');
+
+      /**
+       * Create a new DataChart instance with the given query parameters
+       * and Google chart options. It will be rendered inside an element
+       * with the id "datachert".
+       */
+      /*var dataChart = new gapi.analytics.googleCharts.DataChart({
+        query: {
+          'metrics': 'ga:sessions',
+          'dimensions': 'ga:date',
+          'start-date': '7daysAgo',
+          'end-date': 'yesterday',
+          'ids': "ga:59122748"
+        },
+        chart: {
+          container: 'datachart',
+          type: 'COLUMN',
+          options: {
+            title: 'Users Over The Past 7 Days',
+            width: '100%'
+          }
+        }
+      });*/
+      //dataChart.execute();
     });
 
-    /**
-     * Create a new DataChart instance with the given query parameters
-     * and Google chart options. It will be rendered inside an element
-     * with the id "datachert".
-     */
-    var dataChart = new gapi.analytics.googleCharts.DataChart({
-      query: {
-        'metrics': 'ga:sessions',
-        'dimensions': 'ga:date',
-        'start-date': '7daysAgo',
-        'end-date': 'yesterday',
-        'accountName': "MrMaksimize.com",
-        'propertyName': "MrMaksimize.com",
-        'viewName': "MrMaksimize.com",
-        'ids': "ga:59122748"
-      },
-      chart: {
-        container: 'datachart',
-        type: 'COLUMN',
-        options: {
-          title: 'Users Over The Past 7 Days',
-          width: '100%'
+
+    var panels = {};
+
+    $('.panel').each(function(index, element) {
+      console.log(element);
+      console.log(index);
+
+      var dataChart = new gapi.analytics.googleCharts.DataChart({
+        query: {
+          'metrics': 'ga:sessions',
+          'dimensions': 'ga:date',
+          'start-date': '7daysAgo',
+          'end-date': 'yesterday',
+          'ids': "ga:59122748"
+        },
+        chart: {
+          container: element,
+          type: 'COLUMN',
+          options: {
+            title: 'Users Over The Past 7 Days',
+            width: '100%'
+          }
         }
-      }
-    });
-    dataChart.execute();
-  });
-});
+      }).execute();
+    }); // AuthReady.
+  }); //gapi Ready.
+}); // docReady.
